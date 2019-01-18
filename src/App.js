2c6cfc07a1262dc7db2a34d8e3ebfd398d91b140
trose618 <trose618@gmail.com>
2019-01-18 15:20:11 -0500

    refactored navBar

--- src/App.js
+++ src/App.js
@@ -1,71 +1,84 @@
-import React, { Component } from 'react';
-import './App.css';
-import NavBar from "./components/NavBar"
-import MainPage from './components/MainPage';
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import MainPage from "./components/MainPage";
 import { Route, Switch } from "react-router-dom";
-import ModShowPage from './components/ModShowPage';
-import EditProfileForm from './components/EditProfileForm';
-import LoginForm from './components/LoginForm';
-import SignUpForm from './components/SignUpForm';
+import ModShowPage from "./components/ModShowPage";
+import EditProfileForm from "./components/EditProfileForm";
+import LoginForm from "./components/LoginForm";
+import SignUpForm from "./components/SignUpForm";
 // import StatsDiv from './components/StatsDiv';          will delay this component for later
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    
+
     this.state = {
-      isUserLoggedIn: false,
+      isUserLoggedIn: true,
       currentUser: {
         full_name: "",
         password: "",
         mod_id: 0
       }
-    }
-  
+    };
+
     this.updateHandler = this.updateHandler.bind(this);
-
   }
-
 
   render() {
     return (
-      <div >
+      <div>
         <NavBar isUserLoggedIn={this.state.isUserLoggedIn} />
-        <Switch>
 
+        <Switch>
           <Route path="/home" component={MainPage} />
-          <Route path ="/mod/:id" component={ModShowPage} />
-          <Route path="/editProfile" render={()=>{
-            return(
-            <EditProfileForm currentUser={this.state.currentUser} updateHandler={this.updateHandler} />
-            )}} />
-          <Route path="/login" render={()=>{
-            return (
-            <LoginForm isUserLoggedIn={this.state.isUserLoggedIn} />
-            )}} />
-            <Route path="/signUp" render={()=>{
-            return (
-            <SignUpForm />
-            )}} />
-          
-          
+          <Route path="/mod/:id" component={ModShowPage} />
+          <Route
+            path="/editProfile"
+            render={() => {
+              return (
+                <EditProfileForm
+                  currentUser={this.state.currentUser}
+                  updateHandler={this.updateHandler}
+                />
+              );
+            }}
+          />
+          <Route
+            path="/login"
+            render={() => {
+              return <LoginForm isUserLoggedIn={this.state.isUserLoggedIn} />;
+            }}
+          />
+          <Route
+            path="/signUp"
+            render={() => {
+              return <SignUpForm />;
+            }}
+          />
         </Switch>
-        
-        
+
         {/* {<StatsDiv />} */}
       </div>
     );
   }
 
-  updateHandler(currentUser){
+  updateHandler(currentUser) {
     this.setState({ currentUser });
 
     fetch(`http://localhost:3000/api/v1/users/${currentUser.id}`, {
-      method: "PATCH", 
-      headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
-      body: JSON.stringify({ full_name: currentUser.full_name, password: currentUser.password, mod_id: currentUser.mod_id })
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        full_name: currentUser.full_name,
+        password: currentUser.password,
+        mod_id: currentUser.mod_id
+      })
     });
   }
 }
 
-export default App
+export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,48 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="nav">
+          {this.props.isUserLoggedIn ? (
+            <div>
+              <Link to={"/mod1"}>
+                <li>
+                  <h3>Mod 1</h3>
+                </li>
+              </Link>
 
-  render() { 
-    return ( 
-    <div><h3>NavBar</h3>
+              <Link to={"/mod2"}>
+                <li>
+                  <h3>Mod 2</h3>
+                </li>
+              </Link>
 
+              <Link to={"/mod3"}>
+                <li>
+                  <h3>Mod 3</h3>
+                </li>
+              </Link>
 
-    
-    {/* <ul>
-      if (a user is signed in) {
-        <link to={"/mod1"}>
-        <li><h3>Mod 1</h3></li>
-        </link>
+              <Link to={"/mod4"}>
+                <li>
+                  <h3>Mod 4</h3>
+                </li>
+              </Link>
 
-
-        <link to={"/mod2"}>
-        <li><h3>Mod 2</h3></li>
-        </link>
-
-
-        <link to={"/mod3"}>
-        <li><h3>Mod 3</h3></li>
-        </link>
-
-
-        <link to={"/mod4"}>
-        <li><h3>Mod 4</h3></li>
-        </link>
-
-
-        <link to={"/mod5"}>
-        <li><h3>Mod 5</h3></li>
-        </link>
-
-      } else {
-        
-        <link to={"/LoginSignUpForm"}>
-        <li><h3>Login/SignUp</h3></li>
-        </link>
-        </ul>*/}
-    </div> );
+              <Link to={"/mod5"}>
+                <li>
+                  <h3>Mod 5</h3>
+                </li>
+              </Link>
+            </div>
+          ) : (
+            <Link to={"/LoginSignUpForm"}>
+              <li>
+                <h3>Login/SignUp</h3>
+              </li>
+            </Link>
+          )}
+        </ul>
+      </div>
+    );
   }
 }
- 
-export default NavBar
+
+export default NavBar;
