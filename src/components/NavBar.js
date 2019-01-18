b5c8fde91e730d5320ba4ff0f84fc4eede52ce12
rob <robertchen234@gmail.com>
2019-01-18 14:33:01 -0500

    bring navbar back

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
-  state = { test: x  }
+  state = {
+    
+   }
   render() { 
-    return ( <div>NavBar<hr/></div> );
+    return ( 
+    <div><h3>NavBar</h3>
+
+
+    
+    {/* <ul>
+      if (a user is signed in) {
+        <link to={"/mod1"}>
+        <li><h3>Mod 1</h3></li>
+        </link>
+
+
+        <link to={"/mod2"}>
+        <li><h3>Mod 2</h3></li>
+        </link>
+
+
+        <link to={"/mod3"}>
+        <li><h3>Mod 3</h3></li>
+        </link>
+
+
+        <link to={"/mod4"}>
+        <li><h3>Mod 4</h3></li>
+        </link>
+
+
+        <link to={"/mod5"}>
+        <li><h3>Mod 5</h3></li>
+        </link>
+
+      } else {
+        
+        <link to={"/LoginSignUpForm"}>
+        <li><h3>Login/SignUp</h3></li>
+        </link>
+        </ul>*/}
+    </div> );
   }
 }
  
