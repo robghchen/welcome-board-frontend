e2e66cb5b7b1b4de0d0255df50983ae05a937a82
rob <robertchen234@gmail.com>
2019-01-19 09:40:28 -0500

    navbar needs conditional logic

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 class NavBar extends Component {
   render() {
     return (
+
+      
       <div class="ui menu">
         <Link to={"/mod/1"} class="item">
           Mod 1
