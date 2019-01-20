8acd2ec5de1b53d897864ad2f0cf907107df6067
rob <robertchen234@gmail.com>
2019-01-20 11:01:07 -0500

    beautiful forms

--- src/components/EditProfileForm.js
+++ src/components/EditProfileForm.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { Redirect } from "react-router-dom";
 
 class EditProfileForm extends Component {
@@ -19,7 +19,7 @@ class EditProfileForm extends Component {
 
     render(){
         return (
-            <Fragment>
+            <div id="edit-profile-form" className="ui card form">
                 {this.props.isUserLoggedIn ? (<form onSubmit={this.submitHandler}>
                     <label htmlFor="full_name">Full Name: </label>
                     <input type="text" name="full_name" onChange={this.changeHandler} value={this.state.full_name} />
@@ -29,9 +29,9 @@ class EditProfileForm extends Component {
                     <select name="mod_id" id="mod_id" onChange={this.changeHandler} value={this.state.mod_id}>
                         {this.getMods()}
                     </select>
-                    <button>Update</button>
+                    <input type="submit" className="submit button" value="Update" />
                 </form>) : (<Redirect to="/login" />)}
-            </Fragment>
+            </div>
         );
     }
 
