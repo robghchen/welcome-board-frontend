8962a0ba866fc7a82afd74bdbba278a144a87551
cfernando415 <ca.fernando415@gmail.com>
2019-01-21 10:25:14 -0500

    working on delete for post

--- src/components/PostIt.js
+++ src/components/PostIt.js
@@ -1,18 +1,33 @@
 import React from "react";
 
 class PostIt extends React.Component {
-  componentWillUnmount(){
+  constructor(props){
+    super(props);
 
+    this.state = {
+      markedForDeletion: false
+    }
+
+    this.deleteHandler = this.deleteHandler.bind(this);
+  }
+  componentWillUnmount(){
+    if(markedForDeletion) fetch(`http://localhost:3000/api/v1/posts/${this.props.post.id}`, {method: 'DELETE'})
   }
   render() {
     return (
       <div className="post-wrapper">
         <img alt="" src={require("./post_it_pic.png")} />
         <div className="post-content">
+          <span className="delete">x</span>
           <p>{this.props.post.content}</p>
         </div>
       </div>
     );
+  }
+
+  deleteHandler(){
+    this.setState({ markedForDeletion: true });
+    this.props.deleteHandler(this.props.post);
   }
 }
 
