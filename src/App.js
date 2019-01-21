524d56888cdc37813b184924a7e9ae28bd7bd83c
rob <robertchen234@gmail.com>
2019-01-20 22:00:34 -0500

    login fail goes back to login

--- src/App.js
+++ src/App.js
@@ -194,20 +194,20 @@ class App extends Component {
         mod_id: userInfo.mod_id
       })
     })
-    .then(res => res.json())
-    .then(res => {
-      localStorage.setItem("token", res.jwt);
-      this.setState({
-        isUserLoggedIn: true,
-        token: localStorage.getItem("token"),
-        currentUser: {
-          id: res.user.id,
-          full_name: res.user.full_name,
-          password: "",
-          mod_id: res.user.mod_id
-        }
+      .then(res => res.json())
+      .then(res => {
+        localStorage.setItem("token", res.jwt);
+        this.setState({
+          isUserLoggedIn: true,
+          token: localStorage.getItem("token"),
+          currentUser: {
+            id: res.user.id,
+            full_name: res.user.full_name,
+            password: "",
+            mod_id: res.user.mod_id
+          }
+        });
       });
-    });
   };
 
   submitLoginHandler = (userInfo, event) => {
@@ -229,8 +229,8 @@ class App extends Component {
       })
     })
       .then(res => {
-        if(res.status === 401) throw new Error(res.status)
-        else return res.json()
+        if (res.status === 401) throw new Error(res.status);
+        else return res.json();
       })
       .then(res => {
         localStorage.setItem("token", res.jwt);
@@ -245,7 +245,10 @@ class App extends Component {
           }
         });
       })
-      .catch(error => alert(`HTTP ERROR: ${error}, Unknown account or password!`));
+      .catch(error => {
+        this.props.history.push("/login");
+        alert(`HTTP ERROR: ${error}, Unknown account or password!`);
+      });
   };
 
   logout = () => {
